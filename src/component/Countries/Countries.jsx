8cc74b41d7fb38a046e797beb17c44087eb68e87
flynn-hagidok <flynnhagidok@gmail.com>
2026-04-19 00:25:03 +0600

    second commit

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,15 +1,19 @@
 import { use } from "react"
+import Country from "../Country/Country";
 
-const Countries = ({countriesPromise}) =>{
+const Countries = ({countriesPromise}) => {
 
     const countriesData = use(countriesPromise)
     // console.log(countriesData);
     const countries = countriesData.countries;
-    console.log(countries)
+    // console.log(countries)
 
     return(
         <div>
             <h2>Total Countries: {countries.length}</h2>
+            {
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     )
 }
